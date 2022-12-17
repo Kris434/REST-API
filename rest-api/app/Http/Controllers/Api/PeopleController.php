54ab9e0f93e4c463560cd9e8c93c67eef433b8ca
Krzysztof Bicz <kristofarer@gmail.com>
2022-12-17 20:29:22 +0100

    People Controller
    Kontroler obsługujący przychodzące requesty

---
+++ rest-api/app/Http/Controllers/Api/PeopleController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PeopleResource;
+use App\Models\People;
+use Illuminate\Http\Request;
+
+class PeopleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        $people = People::all();
+        return PeopleResource::collection($people);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $people = People::create($request->all());
+        return new PeopleResource($people);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\People  $people
+     * @return \Illuminate\Http\Response
+     */
+    public function show(People $people)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\People  $people
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(People $people)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\People  $people
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, People $people)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\People  $people
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(People $people)
+    {
+        //
+    }
+}
